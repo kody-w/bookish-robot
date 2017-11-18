625cb64c9fa0046a8a924c69c35f1c07861cb58c
kody-w <kwildfeuer@me.com>
2017-11-18 03:52:49 -0600

    Initial Commit of Treehouse WebAPI Project

---
+++ Treehouse.FitnessFrog.Spa/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using System.Web.Http;
+
+namespace Treehouse.FitnessFrog.Spa
+{
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
